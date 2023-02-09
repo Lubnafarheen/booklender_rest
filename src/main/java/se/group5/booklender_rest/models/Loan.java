ddb9b3d49d13a00171239a40e1163a7485c9f213
lubna farheen <lubna.farheen2010@gmail.com>
2023-02-09 10:06:17 +0100

    Created model Loan class

---
+++ src/main/java/se/group5/booklender_rest/models/Loan.java
@@ -0,0 +1,100 @@
+package se.group5.booklender_rest.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Loan {
+
+    private long loanId;
+
+    private LibraryUser loanTaker;
+
+    private Book book;
+
+    private LocalDate loanDate;
+
+    private boolean concluded;
+
+    public Loan() {
+    }
+
+    public Loan(LibraryUser loanTaker, Book book, LocalDate loanDate, boolean concluded) {
+        this.loanTaker = loanTaker;
+        this.book = book;
+        this.loanDate = loanDate;
+        this.concluded = concluded;
+    }
+
+    public long getLoanId() {
+        return loanId;
+    }
+
+    public LibraryUser getLoanTaker() {
+        return loanTaker;
+    }
+
+    public void setLoanTaker(LibraryUser loanTaker) {
+        this.loanTaker = loanTaker;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public boolean isOverdue(){
+        return true;
+    }
+
+    public BigDecimal getFine(){
+        return null;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public boolean isConcluded() {
+        return concluded;
+    }
+
+    public void setConcluded(boolean concluded) {
+        this.concluded = concluded;
+    }
+
+    public boolean extendLoan (boolean days){
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Loan loan = (Loan) o;
+        return loanId == loan.loanId && concluded == loan.concluded && Objects.equals(loanTaker, loan.loanTaker) && Objects.equals(book, loan.book) && Objects.equals(loanDate, loan.loanDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(loanId, loanTaker, book, loanDate, concluded);
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "loanId=" + loanId +
+                ", loanTaker=" + loanTaker +
+                ", book=" + book +
+                ", loanDate=" + loanDate +
+                ", concluded=" + concluded +
+                '}';
+    }
+}
