364965bbc101ed0c6103fe2006d24056291621d7
lubna farheen <lubna.farheen2010@gmail.com>
2023-02-09 10:15:14 +0100

    Created model Book class

---
+++ src/main/java/se/group5/booklender_rest/models/Book.java
@@ -0,0 +1,110 @@
+package se.group5.booklender_rest.models;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Book {
+
+    private int BookId;
+
+    private String title;
+
+    private boolean available;
+
+    private boolean reserved;
+
+    private int maxLoanDays;
+
+    private BigDecimal finePerDay;
+
+    private String description;
+
+    public Book() {
+    }
+
+    public Book(String title, int maxLoanDays, BigDecimal finePerDay, String description) {
+        this.title = title;
+        this.maxLoanDays = maxLoanDays;
+        this.finePerDay = finePerDay;
+        this.description = description;
+    }
+
+    public int getBookId() {
+        return BookId;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean availableStatus) {
+        this.available = available;
+    }
+
+    public boolean isReserved() {
+        return reserved;
+    }
+
+    public void setReserved(boolean reservedStatus) {
+        this.reserved = reserved;
+    }
+
+    public int getMaxLoanDays() {
+        return maxLoanDays;
+    }
+
+    public void setMaxLoanDays(int maxLoanDays) {
+        this.maxLoanDays = maxLoanDays;
+    }
+
+    public BigDecimal getFinePerDay() {
+        return finePerDay;
+    }
+
+    public void setFinePerDay(BigDecimal finePerDay) {
+        this.finePerDay = finePerDay;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return BookId == book.BookId && available == book.available && reserved == book.reserved && maxLoanDays == book.maxLoanDays && Objects.equals(title, book.title) && Objects.equals(finePerDay, book.finePerDay) && Objects.equals(description, book.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(BookId, title, available, reserved, maxLoanDays, finePerDay, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "BookId=" + BookId +
+                ", title='" + title + '\'' +
+                ", available=" + available +
+                ", reserved=" + reserved +
+                ", maxLoanDays=" + maxLoanDays +
+                ", finePerDay=" + finePerDay +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
