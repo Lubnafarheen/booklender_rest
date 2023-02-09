4692cf9f37c8d58a95b2ee4d08a56c60f79104fb
lubna farheen <lubna.farheen2010@gmail.com>
2023-02-09 09:44:40 +0100

    Project Setup

---
+++ src/main/java/se/group5/booklender_rest/BooklenderRestApplication.java
@@ -0,0 +1,13 @@
+package se.group5.booklender_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklenderRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklenderRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/group5/booklender_rest/BooklenderRestApplicationTests.java
@@ -0,0 +1,13 @@
+package se.group5.booklender_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
